faf2f62e0be3561fea5001048649e1259ef0421d
Guilherme Silva Freire <76671441+Guilhos@users.noreply.github.com>
2024-02-26 17:02:13 -0300

    Update autoCH.py

--- autoCH.py
+++ autoCH.py
@@ -15,17 +15,37 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.errors import HttpError
 from google.oauth2.service_account import Credentials
 
-# Request all access (permission to read/send/receive emails, manage the inbox, and more)
+### 
+# Antes de iniciar o processo de envios de EMAIL, primeiramente verifique se o arquivo 'credentials.json' está na pasta do programa! 
+# Verifique se a API está atualizada e se algum dos métodos utilizados está desatualizado!
+# Verifique se na caixa de GMAIL não tem algum email indesejável!
+# 
+# Para esse código funcionar o email precisa estar no formato:
+# Hora de entrada: hh:mm
+#
+# Hora de saída: hh:mm 
+#
+# É possível alterar este formato mudando o NFC na sede, porém será necessário fazer as mudanças necessárias nos métodos update_values() e read_messages()
+# 
+# ###
+
+
+# Identificação da API do GMAIL
 SCOPES = ['https://mail.google.com/']
-our_email = 'seuemail@gmail.com'
+# GMAIL utilizado
+our_email = 'horadesede@gmail.com'
 
-# If modifying these scopes, delete the file token.json.
+# Identificação da API do SHEETS
 SCOPES1 = ["https://www.googleapis.com/auth/spreadsheets"]
+# O ID do SPREADSHEET.
+SPREADSHEET_ID = "136BgdSmEmHdqMHRvK0dOsgXYjqKdAyicNEtGlMGLw3E"
+# Células que serão alteradas
+RANGE = "A2:D"
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = "your ID"
-SAMPLE_RANGE_NAME = "A1:B2"
+### MÉTODOS ###
 
+# Autenticação da API do GMAIL
+# Depois da primeira autenticação, será criado um arquivo token.pickle, ele servirá como permissão de acesso
 def gmail_authenticate():
     creds = None
     if os.path.exists("token.pickle"):
@@ -35,15 +55,14 @@ def gmail_authenticate():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("file location", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
     return build('gmail', 'v1', credentials=creds)
 
-service = gmail_authenticate()
-
-def sheets_authenticate():
+# Autenticação da API do SHEETS
+def sheet_authenticate():
     creds = None
     if os.path.exists("token1.pickle"):
         with open("token1.pickle", "rb") as token:
@@ -52,35 +71,13 @@ def sheets_authenticate():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("file location", SCOPES1)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES1)
             creds = flow.run_local_server(port=0)
         with open("token1.pickle", "wb") as token:
             pickle.dump(creds, token)
     return build('sheets', 'v4', credentials=creds)
 
-service2 = sheets_authenticate()
-
-def update_values(service2, spreadsheet_id, range_name, value_input_option, _values, deQuem, dia, txtEntrada, txtSaida):
-  try:
-    values = [[deQuem],[dia], [txtEntrada],[txtSaida] ]
-    body = {"majorDimension": "COLUMNS", "values": values}
-    result = (
-        service2.spreadsheets()
-        .values()
-        .append(
-            spreadsheetId=spreadsheet_id,
-            range= range_name,
-            valueInputOption=value_input_option,
-            body=body,
-        )
-        .execute()
-    )
-    print(f"{(result.get('updates').get('updatedCells'))} cells appended.")
-    return result
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    return error
-
+# Procurar EMAIL de acordo com a QUERY
 def search_messages(service, query):
     result = service.users().messages().list(userId='me',q=query).execute()
     messages = [ ]
@@ -92,10 +89,8 @@ def search_messages(service, query):
         if 'messages' in result:
             messages.extend(result['messages'])
     return messages
-
-def clean(text):
-    return "".join(c if c.isalnum() else "_" for c in text)
-                                    
+                            
+# Pegar dados do EMAIL        
 def read_message(service, message):
     msg = service.users().messages().get(userId='me', id=message['id'], format='full').execute()
     payload = msg['payload']
@@ -104,17 +99,19 @@ def read_message(service, message):
     folder_name = "email"
     has_subject = False
     
+    # Aqui estamos coletando o nome do REMETENTE e a DATA
     if headers:
         for header in headers:
             name = header.get("name")
             value = header.get("value")
             if name.lower() == 'from':
-                dq = value.split()
-                deQuem = dq[0] + dq[1]
+                # REMETENTE
+                rm = value.split()
+                remetente = rm[0] + rm[1]
                 print("From:", value)
             if name.lower() == "to":
                 print("To:", value)
-            if name.lower() == "subject":
+            if name.lower() == "subject": # Sinceramente NÃO SEI direito o que essa parte faz, MAS NÃO EXCLUA
                 has_subject = True
                 folder_name = clean(value)
                 folder_counter = 0
@@ -128,10 +125,12 @@ def read_message(service, message):
                         folder_name = f"{folder_name}_{folder_counter}"
                 print("Subject:", value)
             if name.lower() == "date":
+                # DATA
                 print("Date:", value)
                 calendario = value.split()
                 dia = calendario[1]+"/"+calendario[2]+"/"+calendario[3]
     
+    # Aqui se coleta a HORA DE ENTRADA e HORA DE SAÍDA
     if parts:
         for part in parts:
             mimeType = part.get("mimeType")
@@ -144,20 +143,37 @@ def read_message(service, message):
                     text = urlsafe_b64decode(data).decode()
                     print(text)
                     textao = text.split()
-                    txtEntrada = textao[3]
-                    txtSaida = textao[7]
+                    txtEntrada = textao[3] # ENTRADA
+                    txtSaida = textao[7] # SAÍDA
+                    
+                    # OBSERVAÇÃO: Estou usando estes index, pois de acordo com o formato do EMAIL (mostrado nos comentários
+                    # lá de cima), as horas ficam nessas exatas posições! Então não foi necessário fazer um algoritmo de
+                    # busca ou algo do tipo
     
-    if __name__ == "__main__":
-        main()
-        update_values(service2,
-      SAMPLE_SPREADSHEET_ID,
-      SAMPLE_RANGE_NAME,
-      "USER_ENTERED",
-      [[], []], deQuem,dia, txtEntrada,txtSaida
-        )
+    info = [remetente, dia, txtEntrada, txtSaida]
+    return info
     
     print("="*50)
-    
+
+# Colocar informações na SPREADSHEET
+def update_values(service, spreadsheet_id, range_name, value_input_option, _values, remetente, data, Entrada, Saida):
+    try:
+        values = [[remetente],[data],[Entrada],[Saida]]
+        body = {"majorDimension": "COLUMNS", "values": values}
+        result = (service.spreadsheets().values().append(
+            spreadsheetId=spreadsheet_id,
+            range= range_name,
+            valueInputOption=value_input_option,
+            body=body,
+        ).execute()
+    )
+        print(f"{(result.get('updates').get('updatedCells'))} cells appended.")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+# Deletar EMAIL usado
 def delete_messages(service, query):
     messages_to_delete = search_messages(service, query)
     return service.users().messages().batchDelete(
@@ -166,10 +182,26 @@ def delete_messages(service, query):
           'ids': [ msg['id'] for msg in messages_to_delete]
       }
     ).execute()
-    
-results = search_messages(service, "Hora de entrada")
+
+#  Limpa o texto em read_message() para criar uma nova pasta
+def clean(text):
+    return "".join(c if c.isalnum() else "_" for c in text)
+
+### PROGRAMA ###
+
+sheet_serv = sheet_authenticate()
+gmail_serv = gmail_authenticate()
+
+# Aqui se coloca a QUERY
+results = search_messages(gmail_serv, "Hora de entrada")
 print(f"Found {len(results)} results.")
-for msg in results:
-    read_message(service, msg)
-#delete_messages(service, "Hora de entrada")
+
+# Aqui se vai ler o EMAIL e coloca-lo no SPREADSHEET
+for i in range(len(results)-1, -1, -1):
+    msg = results[i]
+    info = read_message(gmail_serv, msg)
+    update_values(sheet_serv, SPREADSHEET_ID, RANGE, "USER_ENTERED",[[], []], info[0], info[1], info[2], info[3])
+ 
+# Deleta o EMAIL para que não seja lido 2 vezes       
+delete_messages(gmail_serv, "Hora de entrada")
 
